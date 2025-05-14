812a501093e802c44ad7e7e5fc84c61ea3a8ffd1
buyili <1132989278@qq.com>
2025-05-14 13:52:42 +0800

    Update

--- lib/utils/setup.dart
+++ lib/utils/setup.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:adb_tools/providers/app_provider.dart';
 import 'package:adb_tools/utils/const.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SteupUtils {
   static final String _appPath = Platform.resolvedExecutable;
